2d2e34f19ce866fd012fc9cd731335094ce70ade
RASH\Mobby <rashadjavad@gmail.com>
2017-02-02 17:32:23 +0400

    v1.1

--- src/main/java/rashjz/info/MainLayout.java
+++ src/main/java/rashjz/info/MainLayout.java
@@ -13,16 +13,20 @@ public class MainLayout extends MainLayoutDesign {
     public MainLayout(CustomerRepository repository) {
         this.repository = repository;
         Navigator navigator = new Navigator(UI.getCurrent(), contentPanel);
+        navigator.setErrorView(ErrorView.class);
         navigator.addView(StatsView.VIEW_NAME, new StatsView(repository));
         navigator.addView(PluginsView.VIEW_NAME, PluginsView.class);
         navigator.addView(PermissionsView.VIEW_NAME, PermissionsView.class);
+        navigator.addView(LoginPage.NAME, LoginPage.class);
+
+
 
         menuButton1.addClickListener(event -> doNavigate(StatsView.VIEW_NAME));
         menuButton2.addClickListener(event -> doNavigate(PluginsView.VIEW_NAME));
         menuButton3.addClickListener(event -> doNavigate(PermissionsView.VIEW_NAME));
 
         if (navigator.getState().isEmpty()) {
-            navigator.navigateTo(StatsView.VIEW_NAME);
+            navigator.navigateTo(LoginPage.NAME);
         } else {
             navigator.navigateTo(navigator.getState());
         }
